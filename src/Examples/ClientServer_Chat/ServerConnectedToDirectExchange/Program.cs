f027c4cf7a035c2cd581bf2a86fd00e86dc55424
MKashifJaved <MKashifJaved@DESKTOP-HN1QNFN>
2025-01-07 02:41:41 +0500

    Unnecessary example removed.

--- src/Examples/ClientServer_Chat/ServerConnectedToDirectExchange/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using Core.Commons;
-
-var channel = RabbitMqDirectExchangeHelper.CreateRabbitMqChannel();
-
-var message = "This is a message to publish.";
-var routeKey = "";
-
-RabbitMqDirectExchangeHelper.BasicPublishMessage(channel, routeKey, message);
