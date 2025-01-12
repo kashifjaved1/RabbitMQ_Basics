a1eb0e07ee4f7819d79eac57487065a586c82fc8
MKashifJaved <MKashifJaved@DESKTOP-HN1QNFN>
2025-01-13 02:43:21 +0500

    Headers exchange example added.

---
+++ src/Examples/HeadersExchange/Consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using Core.Commons.Enums;
+using Core.Commons.Helpers;
+using Core.Commons.Shared;
+
+Uri uri = new Uri("amqp://guest:guest@localhost:5672");
+string clientName = "Consumer";
+
+var connectionFactory = RabbitMqHelper.GetConnectionFactory(uri, clientName);
+var connection = RabbitMqHelper.OpenConnection(connectionFactory);
+var channel = RabbitMqHelper.CreateChannel(connectionFactory, connection);
+
+var exchagneName = ExampleData.GetCustomExchangeName("with_headers");
+RabbitMqHelper.CreateExchange(channel, exchagneName, ExchangeType.Headers);
+
+var queueName = ExampleData.GetCustomQueueName(1);
+RabbitMqHelper.CreateQueue(channel, queueName);
+
+var bindingArgs = new Dictionary<string, object>
+{
+    //{ "x-match", "all" }, // means if all header properties of headerExchange matches with this queue then it'll receive the data.
+    { "x-match", "any" }, // means if any header propertie of headerExchange matches with this queue then it'll receive the data.
+    { "name", "abc" },
+    { "rank", "xyz" }
+};
+
+RabbitMqHelper.BindQueue(channel, queueName, exchagneName, string.Empty, arguments: bindingArgs);
+
+var consumer = RabbitMqHelper.GetConsumer(channel);
+RabbitMqHelper.BasicConsumeMessage(consumer, channel, queueName);
+
+Console.ReadLine();
+RabbitMqHelper.CloseConnection(channel, connection);
---
+++ src/Examples/HeadersExchange/Producer/Program.cs
@@ -0,0 +1,25 @@
+﻿using Core.Commons.Enums;
+using Core.Commons.Helpers;
+using Core.Commons.Shared;
+
+Uri uri = new Uri("amqp://guest:guest@localhost:5672");
+string clientName = "Publisher";
+
+var connectionFactory = RabbitMqHelper.GetConnectionFactory(uri, clientName);
+var connection = RabbitMqHelper.OpenConnection(connectionFactory);
+var channel = RabbitMqHelper.CreateChannel(connectionFactory, connection);
+
+var exchangeName = ExampleData.GetCustomExchangeName("with_headers");
+RabbitMqHelper.CreateExchange(channel, exchangeName, ExchangeType.Headers);
+
+var message = "This is a message with headers!";
+var properties = channel.CreateBasicProperties();
+properties.Headers = new Dictionary<string, object>
+{
+    { "name", "abc" }
+};
+
+RabbitMqHelper.BasicPublishMessage(channel, exchange: exchangeName, routingKey: string.Empty, message: message, properties: properties);
+
+Console.ReadKey();
+RabbitMqHelper.CloseConnection(channel, connection);
